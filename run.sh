f88d5399f4c655212d6ee4f3daf797f94e0c633c
Rainmaker <obioraakubue@yahoo.com>
2020-11-08 05:23:08 -0500

    customize config

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
+echo "$AWS_ACCESS_KEY:$AWS_SECRET_ACCESS_KEY" > passwd && chmod 600 passwd
 s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd  && tail -f /dev/null
